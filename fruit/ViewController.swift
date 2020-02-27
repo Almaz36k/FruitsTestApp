76c0636ac9f271d7c73f9f30ee59d0a665295fd0
Almaz <aksakova53@gmail.com>
2020-02-27 20:06:41 +0300

    Initial Commit

---
+++ fruit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  fruit
+//
+//  Created by MacBook on 27.02.2020.
+//  Copyright © 2020 MacPro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
